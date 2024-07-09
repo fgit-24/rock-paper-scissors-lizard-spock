0b8c501b2ad2d9bfbea2d8c97c051d1b2e5b5106
Florian Hass <f91hama@gmail.com>
2024-07-09 20:49:07 +0000

    04 - Add const variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const choices = ["rock", "paper", "scissors", "liyard", "spock"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("playerDisplay");
+const resultDisplay = document.getElementById("playerDisplay");
