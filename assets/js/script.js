d6aaf5174c8fad198574d9b3dd60c6fb28f3464f
Florian Hass <f91hama@gmail.com>
2024-07-10 06:53:30 +0000

    05 - Add if-statment with switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,32 @@ const choices = ["rock", "paper", "scissors", "liyard", "spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("playerDisplay");
 const resultDisplay = document.getElementById("playerDisplay");
+
+
+if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result =
+          computerChoice === "scissors" || "lizard" ? "You win!" : "You lose!";
+        break;
+      case "paper":
+        result =
+          computerChoice === "rock" || "lizard" ? "You win!" : "You lose!";
+        break;
+      case "scissors":
+        result =
+          computerChoice === "paper" || "lizard" ? "You win!" : "You lose!";
+        break;
+      case "lizard":
+        result =
+          computerChoice === "paper" || "lizard" ? "You win!" : "You lose!";
+        break;
+      case "spock":
+        result =
+          computerChoice === "paper" || "lizard" ? "You win!" : "You lose!";
+        break;
+    }
+}
+
